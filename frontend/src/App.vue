efe191e9f400b83591bdb3f52ac5028b70f08e80
Douglas Fernandes <douglas.fernandes91@live.com>
2024-04-21 15:53:31 -0300

    feat: add card component to best UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,14 @@ import {
   AccordionItem,
   AccordionTrigger
 } from './components/ui/accordion';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter
+} from './components/ui/card'
 import { Button } from './components/ui/button';
 import { Input } from './components/ui/input'
 import { ref } from 'vue';
@@ -16,8 +24,21 @@ const datas = ref<IPostalCode[]>([]);
 const cepInput = ref('');
 const city = ref('');
 
+function formatCep() {
+  cepInput.value = cepInput.value.replace(/\D/g, '');
+  if (cepInput.value.length > 5) {
+    cepInput.value = cepInput.value.replace(/^(\d{5})(\d{0,3}).*/, '$1-$2');
+  }
+}
+
+
 async function searchAddressByPostalCode(cep: string) {
-  const service = await new AxiosHttpService().get<AxiosResponse>(`cep/${cep}`);
+  if (!cep) {
+    alert('Por favor, preencha o campo de CEP.');
+    return;
+  }
+
+  const service = await new AxiosHttpService().get<AxiosResponse>(`cep/${cep.replace(/\D/g, '')}`);
 
   if (service.status !== 200) return alert('Não foi');
 
@@ -32,21 +53,32 @@ async function searchAddressByPostalCode(cep: string) {
 
 
 <template>
-  <div>
-    <div class="flex w-full justify-center">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+  <div class="flex w-full justify-center">
+    <div>
+      <div class="w-fit">
+        <img src="./assets/image.png" class="logo vue" alt="Vue logo" />
+      </div>
     </div>
-    <Input id="postalCode" type="text" placeholder="00000-000" v-model="cepInput" />
-    <Button id="searchAddressButton" variant="outline" @click="searchAddressByPostalCode(cepInput)"
-      class="flex w-full justify-center">
-      Consultar Cep
-    </Button>
-    <Accordion type="single" class="w-full">
+    <Card class="w-full max-w-lg mx-auto px-4 mt-10">
+      <CardHeader class="mb-4">
+        <CardTitle>Encontre seu Endereço</CardTitle>
+        <CardDescription>Utilitário para você encontrar seu endereço</CardDescription>
+      </CardHeader>
+      <CardContent class="flex flex-col items-center">
+        <Input class="text-black mb-4" id="postalCode" type="string" placeholder="00000-000" @input="formatCep"
+          v-model="cepInput" />
+        <Button id="searchAddressButton" variant="outline" @click="searchAddressByPostalCode(cepInput)"
+          class="font-bold py-2 px-4 rounded hover:bg-blue-700 hover:text-white text-black">
+          Consultar Cep
+        </Button>
+      </CardContent>
+      <CardFooter>
+        Card Footer
+      </CardFooter>
+    </Card>
+  </div>
+  <div class="flex justify-center mt-14">
+    <Accordion type="single" class="w-80">
       <AccordionItem v-for="({ data }, id) in datas" :key="id + 1" :value="data.title">
         <AccordionTrigger class="address-title"> {{ data.address.city }} </AccordionTrigger>
         <AccordionContent> {{ id + 1 }}|{{ data.address.countryCode }} </AccordionContent>
