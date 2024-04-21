a690117ac7dd813abbd92cc4632e4fb5cb1043cd
Douglas Fernandes <douglas.fernandes91@live.com>
2024-04-21 17:24:54 -0300

    feat: separate CEP component

---
+++ frontend/src/components/views/cepview/CepView.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '../../ui/accordion';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter
+} from '../../ui/card'
+import { Button } from '../../ui/button';
+import { Input } from '../../ui/input'
+import { ref } from 'vue';
+import { IPostalCode } from '@interface/IPostalCode';
+import { AxiosResponse } from 'axios';
+import { AxiosHttpService } from '../../../service/Http/AxiosHttpService';
+
+const postalAddress = ref<IPostalCode[]>([]);
+const cepInput = ref('');
+
+function formatCep() {
+  cepInput.value = cepInput.value.replace(/\D/g, '');
+  if (cepInput.value.length > 5) {
+    cepInput.value = cepInput.value.replace(/^(\d{5})(\d{0,3}).*/, '$1-$2');
+  }
+}
+
+async function searchAddressByPostalCode(cep: string) {
+  if (!cep) {
+    alert('Por favor, preencha o campo de CEP.');
+    return;
+  }
+
+  const service = await new AxiosHttpService().get<AxiosResponse>(`cep/${cep.replace(/\D/g, '')}`);
+
+  if (service.status !== 200) return alert('Não foi');
+
+  const { data } = service.data as IPostalCode;
+
+  postalAddress.value.push({ data });
+};
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <div>
+      <div class="w-fit">
+        <img src="../../../assets/image.png" class="logo vue" alt="Vue logo" />
+      </div>
+    </div>
+    <Card class="w-full max-w-lg mx-auto px-4 mt-10">
+      <CardHeader class="mb-4">
+        <CardTitle>Encontre seu Endereço</CardTitle>
+        <CardDescription>Utilitário para você encontrar seu endereço</CardDescription>
+      </CardHeader>
+      <CardContent class="flex flex-col items-center">
+        <Input class="text-black mb-4" id="postalCode" type="string" placeholder="00000-000" @input="formatCep"
+          v-model="cepInput" />
+        <Button id="searchAddressButton" variant="outline" @click="searchAddressByPostalCode(cepInput)"
+          class="font-bold py-2 px-4 rounded hover:bg-blue-700 hover:text-white text-black">
+          Consultar Cep
+        </Button>
+      </CardContent>
+      <CardFooter>
+        Card Footer
+      </CardFooter>
+    </Card>
+  </div>
+  <div class="flex justify-center mt-14">
+    <Accordion type="single" class=" w-2/6">
+      <AccordionItem v-for="({ data }, id) in postalAddress" :key="id + 1" :value="data.title">
+        <AccordionTrigger class="address-title mr-6">
+          #{{ id + 1 }} | {{ data.address.city }}
+          <span class="border-solid border-2 border-sky-500 rounded-md mr-4">
+            latitude: <span class="flex flec-col px-4"> {{ data.position.lat }}</span>
+          </span>
+          <span class="border-solid border-2 border-sky-500 rounded-md mr-2">
+            longitude <span class="flex flec-col px-4"> {{ data.position.lng }} </span>
+          </span>
+        </AccordionTrigger>
+        <AccordionContent>{{ data.title }} </AccordionContent>
+        <AccordionContent>{{ data.address.countryCode }} </AccordionContent>
+        <AccordionContent>{{ data.address.state }} | {{ data.address.stateCode }} </AccordionContent>
+        <AccordionContent v-if="data.address.district && data.address.district.trim() !== ''">
+          {{ data.address.district }}
+        </AccordionContent>
+        <AccordionContent>
+          {{ data.mapView.west }} | {{ data.mapView.south }} | {{ data.mapView.east }} | {{ data.mapView.north }}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ frontend/src/components/views/cepview/index.ts
@@ -0,0 +1,1 @@
+export { default as CepView } from './CepView.vue'
