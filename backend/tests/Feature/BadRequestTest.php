6fcc2a2e4240e18db1cf46ba4335706a10256cde
Douglas Fernandes <douglas.fernandes91@live.com>
2024-04-21 21:45:05 -0300

    fix: change bad request test

--- backend/tests/Feature/BadRequestTest.php
+++ backend/tests/Feature/BadRequestTest.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PostalCodeProviderController;
 use App\Services\PostalCodeProviderService;
 use Illuminate\Support\Facades\Http;
 
-it('returns status 500', function () {
+it('returns status 400 BadRequest', function () {
   Http::fake([
     'https://geocode.search.hereapi.com/v1/geocode?q=01512010&apiKey=be1hHQrytN0izt1ZAfwMHj5NLwdG0AHGBBb13FUjcGk' => Http::response([], 200),
   ]);
@@ -13,5 +13,5 @@ it('returns status 500', function () {
 
   $response = $controller->getAddresByPostalCode(00000000);
 
-  $this->assertEquals(500, $response->getStatusCode());
+  $this->assertEquals(400, $response->getStatusCode());
 });
